ae1975d4cd7cf9537ea39eeef7dee98b6196224d
diamond <aaron.diamond2@yahoo.co.nz>
2023-11-19 00:26:17 +1300

    partially finished the styling of the navigation

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,9 +1,70 @@
 import React from "react";
+import {
+  Basketball,
+  Code,
+  Gavel,
+  Newspaper,
+  PawPrint,
+  GlobeHemisphereEast,
+  List,
+} from "@phosphor-icons/react";
+import YoutubeLogo from "../../assets/img/youtubelogo.png";
 
+import "../../styles/navstyles.css";
 const Nav = () => {
   return (
     <>
-      <p>NAV</p>
+      <div className="nav-wrapper__container">
+        <div className="menu-logo__wrapper">
+          <div>
+            <List size={24} color="rgba(244,244,244,0.7)" />
+          </div>
+          <img
+            className="youtube-logo__img"
+            src={YoutubeLogo}
+            alt="youtubeLogo"
+          />
+        </div>
+        <ul>
+          <button>
+            <li>
+              <Basketball size={20} color="rgba(244,244,244,0.7)" /> &nbsp;
+              &nbsp; Sports
+            </li>
+          </button>
+          <button>
+            <li>
+              <Code size={20} color="rgba(244,244,244,0.7)" /> &nbsp; &nbsp;
+              Coding
+            </li>
+          </button>
+
+          <button>
+            <li>
+              <Gavel size={20} color="rgba(244,244,244,0.7)" /> &nbsp; &nbsp;
+              Politics
+            </li>
+          </button>
+          <button>
+            <li>
+              <Newspaper size={20} color="rgba(244,244,244,0.7)" /> &nbsp;
+              &nbsp; News
+            </li>
+          </button>
+          <button>
+            <li>
+              <PawPrint size={20} color="rgba(244,244,244,0.7)" /> &nbsp; &nbsp;
+              Animals
+            </li>
+          </button>
+          <button>
+            <li>
+              <GlobeHemisphereEast size={20} color="rgba(244,244,244,0.7)" />{" "}
+              &nbsp; &nbsp; Planet
+            </li>
+          </button>
+        </ul>
+      </div>
     </>
   );
 };
